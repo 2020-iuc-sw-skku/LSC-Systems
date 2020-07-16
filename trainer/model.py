8d1f733821e1fc763ed9d5b537f7e265734f4c68
Jaemin Choi <1dotolee@gmail.com>
2020-07-16 11:24:36 +0900

    Create model generator to dump models
    학습 전 model을 trainer에 dump함

---
+++ trainer/model.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+from joblib import dump
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from setup import *
+
+
+def dump_model(model, name):
+    PATH_PREDICT = CONFIG["PATH"]["PATH_PREDICT"]
+    dump(model, os.path.join(PATH_PREDICT, name))
+
+
+def create_lr():
+    model_lr = LogisticRegression(
+        multi_class="ovr", penalty="l2", solver="liblinear", random_state=42, n_jobs=-1,
+    )
+    dump_model(model_lr, "model_lr")
+
+
+def create_rf():
+    model_rf = RandomForestClassifier(
+        n_estimators=100,
+        criterion="gini",
+        max_features="sqrt",
+        random_state=42,
+        n_jobs=1,
+    )
+    dump_model(model_rf, "model_rf")
+
+
+def create_gbm():
+    model_gbm = GradientBoostingClassifier(
+        n_estimators=100, loss="deviance", criterion="friedman_mse", random_state=42
+    )
+    dump_model(model_gbm, "model_gbm")
+
+
+def create_ann():
+    model_ann = MLPClassifier(
+        activation="relu", hidden_layer_sizes=(100,), solver="adam"
+    )
+    dump_model(model_ann, "model_ann")
+
+
+if __name__ == "__main__":
+    create_lr()
+    create_rf()
+    create_gbm()
+    create_ann()
